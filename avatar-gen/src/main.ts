059d6e6e7ae21ecc1d1964870280aa0b551b6a88
Hladikes <adi.hladik@gmail.com>
2024-05-10 17:23:33 +0200

    added body part generators

--- avatar-gen/src/main.ts
+++ avatar-gen/src/main.ts
@@ -1,7 +1,7 @@
 import './style.css'
 
-const WIDTH = 400
-const HEIGHT = 400
+const WIDTH = 300
+const HEIGHT = 300
 
 const canvas = document.createElement('canvas')
 canvas.width = WIDTH
@@ -27,25 +27,117 @@ const PUPIL = {
   height: EYE.height / 3,
 }
 
-ctx.fillStyle = '#DDD'
-ctx.fillRect((WIDTH / 2) - (HEAD.width / 2), HEAD.top, HEAD.width, HEAD.height)
+const body = {
+  head: [
+    (ctx: CanvasRenderingContext2D, params: {}) => {
+      // Head
+      ctx.fillStyle = '#DDD'
+      ctx.fillRect((WIDTH / 2) - (HEAD.width / 2), HEAD.top, HEAD.width, HEAD.height)
+    }
+  ],
+  eyes: [
+    (ctx: CanvasRenderingContext2D, params: { eyebrowColor: string }) => {
+      // Eyes
+      ctx.fillStyle = '#FFF'
+      ctx.fillRect((WIDTH / 2) - (EYE.gap / 2), HEAD.top + 30, -EYE.width, EYE.height)
+      ctx.fillRect((WIDTH / 2) + (EYE.gap / 2), HEAD.top + 30, EYE.width, EYE.height)
+  
+      // Eyebrows
+      ctx.fillStyle = params.eyebrowColor
+      ctx.fillRect((WIDTH / 2) - (EYE.gap / 2), HEAD.top + 30, -EYE.width, EYE.height * (2 / 3))
+      ctx.fillRect((WIDTH / 2) + (EYE.gap / 2), HEAD.top + 30, EYE.width, EYE.height * (2 / 3))
+      
+      // Pupils
+      ctx.fillStyle = '#000'
+      ctx.fillRect((WIDTH / 2) - (EYE.gap / 2) - PUPIL.width, HEAD.top + 30 + EYE.height * (2 / 3), -PUPIL.width, PUPIL.height)
+      ctx.fillRect((WIDTH / 2) + (EYE.gap / 2) + PUPIL.width, HEAD.top + 30 + EYE.height * (2 / 3), PUPIL.width, PUPIL.height)
+    },
+    (ctx: CanvasRenderingContext2D, params: { eyebrowColor: string }) => {
+      // Eyes
+      ctx.fillStyle = '#FFF'
+      ctx.fillRect((WIDTH / 2) - (EYE.gap / 2), HEAD.top + 30, -EYE.width, EYE.height)
+      ctx.fillRect((WIDTH / 2) + (EYE.gap / 2), HEAD.top + 30, EYE.width, EYE.height)
+  
+      // Eyebrows
+      ctx.fillStyle = params.eyebrowColor
+      ctx.fillRect((WIDTH / 2) - (EYE.gap / 2), HEAD.top + 30, -EYE.width, EYE.height * (1 / 3))
+      ctx.fillRect((WIDTH / 2) + (EYE.gap / 2), HEAD.top + 30, EYE.width, EYE.height * (1 / 3))
+      
+      // Pupils
+      ctx.fillStyle = '#000'
+      ctx.fillRect((WIDTH / 2) - (EYE.gap / 2) - PUPIL.width, HEAD.top + 30 + EYE.height * (2 / 3), -PUPIL.width, -PUPIL.height)
+      ctx.fillRect((WIDTH / 2) + (EYE.gap / 2) + PUPIL.width, HEAD.top + 30 + EYE.height * (2 / 3), PUPIL.width, -PUPIL.height)
+    },
+    (ctx: CanvasRenderingContext2D, params: { eyebrowColor: string }) => {
+      // Eyes
+      ctx.fillStyle = '#FFF'
+      ctx.fillRect((WIDTH / 2) - (EYE.gap / 2), HEAD.top + 30, -EYE.width, EYE.height)
+      ctx.fillRect((WIDTH / 2) + (EYE.gap / 2), HEAD.top + 30, EYE.width, EYE.height)
+  
+      // Eyebrows
+      ctx.fillStyle = params.eyebrowColor
+      ctx.fillRect((WIDTH / 2) - (EYE.gap / 2), HEAD.top + 30, -EYE.width, EYE.height * (2 / 3))
+      ctx.fillRect((WIDTH / 2) + (EYE.gap / 2), HEAD.top + 30, EYE.width, EYE.height * (1 / 3))
+      
+      // Pupils
+      ctx.fillStyle = '#000'
+      ctx.fillRect((WIDTH / 2) - (EYE.gap / 2), HEAD.top + 30 + EYE.height * (2 / 3), -PUPIL.width, PUPIL.height)
+      ctx.fillRect((WIDTH / 2) + (EYE.gap / 2) + (PUPIL.width * 2), HEAD.top + 30 + EYE.height, PUPIL.width, -PUPIL.height * 2)
+    },
+    (ctx: CanvasRenderingContext2D, params: { eyebrowColor: string }) => {
+      // Eyes
+      ctx.fillStyle = '#FFF'
+      ctx.fillRect((WIDTH / 2) - (EYE.gap / 2), HEAD.top + 30, -EYE.width, EYE.height)
+      ctx.fillRect((WIDTH / 2) + (EYE.gap / 2), HEAD.top + 30, EYE.width, EYE.height)
+  
+      // Eyebrows
+      ctx.fillStyle = params.eyebrowColor
+      ctx.fillRect((WIDTH / 2) - (EYE.gap / 2), HEAD.top + 30, -EYE.width, EYE.height * (2 / 3))
+      ctx.fillRect((WIDTH / 2) + (EYE.gap / 2), HEAD.top + 30, EYE.width, EYE.height * (1 / 3))
+      
+      // Pupils
+      ctx.fillStyle = '#000'
+      ctx.fillRect((WIDTH / 2) - (EYE.gap / 2), HEAD.top + 30 + EYE.height * (2 / 3), -PUPIL.width, PUPIL.height)
+      ctx.fillRect((WIDTH / 2) + (EYE.gap / 2) + (PUPIL.width * 2), HEAD.top + 30 + EYE.height, PUPIL.width, -PUPIL.height * 2)
+    },
+    (ctx: CanvasRenderingContext2D, params: { eyebrowColor: string }) => {
+      // Eyes
+      ctx.fillStyle = '#FFF'
+      ctx.fillRect((WIDTH / 2) - (EYE.gap / 2), HEAD.top + 30, -EYE.width, EYE.height)
+      ctx.fillRect((WIDTH / 2) + (EYE.gap / 2), HEAD.top + 30, EYE.width, EYE.height)
+      
+      // Eyebrows
+      ctx.fillStyle = params.eyebrowColor
+      ctx.fillRect((WIDTH / 2) - (EYE.gap / 2), HEAD.top + 30, -EYE.width, EYE.height * (1 / 3))
+      ctx.fillRect((WIDTH / 2) + (EYE.gap / 2), HEAD.top + 30, EYE.width, EYE.height * (2 / 3))
+      
+      // Pupils
+      ctx.fillStyle = '#000'
+      ctx.fillRect((WIDTH / 2) - (EYE.gap / 2) - (PUPIL.width * 2), HEAD.top + 30 + EYE.height, -PUPIL.width, -PUPIL.height * 2)
+      ctx.fillRect((WIDTH / 2) + (EYE.gap / 2), HEAD.top + 30 + EYE.height * (2 / 3), PUPIL.width, PUPIL.height)
+    },
+  ],
+  mouth: [
+    (ctx: CanvasRenderingContext2D, params: {}) => {
+      // Mouth added
+      ctx.fillStyle = '#AAA'
+      ctx.fillRect((WIDTH / 2) - (EYE.gap / 2) - EYE.width, HEAD.top + 30 + EYE.height + 10, EYE.width * 2 + EYE.gap, 3)
+    }
+  ],
+  legs: [
+    (ctx: CanvasRenderingContext2D, params: {}) => {
+      // Legs
+      ctx.fillStyle = '#222'
+      ctx.fillRect(WIDTH / 2 + 15, HEAD.height + HEAD.top, -3, 1000)
+      ctx.fillRect(WIDTH / 2 - 15, HEAD.height + HEAD.top, 3, 1000)
+    }
+  ],
+}
 
-ctx.fillStyle = '#FFF'
-ctx.fillRect((WIDTH / 2) - (EYE.gap / 2), HEAD.top + 30, -EYE.width, EYE.height)
-ctx.fillRect((WIDTH / 2) + (EYE.gap / 2), HEAD.top + 30, EYE.width, EYE.height)
 
-ctx.fillStyle = '#BBB'
-ctx.fillRect((WIDTH / 2) - (EYE.gap / 2), HEAD.top + 30, -EYE.width, EYE.height * (2 / 3))
-ctx.fillRect((WIDTH / 2) + (EYE.gap / 2), HEAD.top + 30, EYE.width, EYE.height * (2 / 3))
-
-ctx.fillStyle = '#000'
-ctx.fillRect((WIDTH / 2) - (EYE.gap / 2) - PUPIL.width, HEAD.top + 30 + EYE.height * 2 / 3, -PUPIL.width, PUPIL.height)
-ctx.fillRect((WIDTH / 2) + (EYE.gap / 2) + PUPIL.width, HEAD.top + 30 + EYE.height * 2 / 3, PUPIL.width, PUPIL.height)
-
-ctx.fillStyle = '#AAA'
-ctx.fillRect((WIDTH / 2) - (EYE.gap / 2) - EYE.width, HEAD.top + 30 + EYE.height + 10, EYE.width * 2 + EYE.gap, 3)
-
-ctx.fillRect(WIDTH / 2 + 15, HEAD.height + HEAD.top, -3, 1000)
-ctx.fillRect(WIDTH / 2 - 15, HEAD.height + HEAD.top, 3, 1000)
+body.head[0](ctx, {})
+body.eyes[4](ctx, { eyebrowColor: '#BBB' })
+body.mouth[0](ctx, {})
+body.legs[0](ctx, {})
 
 document.body.appendChild(canvas)
