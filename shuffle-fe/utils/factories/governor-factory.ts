566d9a4ed3867ff6f868ca0a8cb6c612c25d4f7d
Branislav Hozza <brankohozza@gmail.com>
2024-05-10 19:59:09 +0200

    :sparkles: prepare dao and governor factory

---
+++ shuffle-fe/utils/factories/governor-factory.ts
@@ -0,0 +1,40 @@
+import { abi as shuffleGovernorABI } from "@/abi/ShuffleGovernor";
+
+const shuffleGovernorContract = {
+  address: "0x0000000000000000000000000000000000000000",
+  abi: shuffleGovernorABI,
+} as const;
+
+export const useDaoFactory = () => {
+  const getProposalThreshold = () =>
+    useReadContract({
+      ...shuffleGovernorContract,
+      functionName: "proposalThreshold",
+      args: [],
+    });
+
+  const getQuorum = (blockNumber: string) => {
+    const _blockNumber = BigInt(blockNumber);
+    useReadContract({
+      ...shuffleGovernorContract,
+      functionName: "quorum",
+      args: [_blockNumber],
+    });
+  };
+
+  const getVotingPeriod = () =>
+    useReadContract({
+      ...shuffleGovernorContract,
+      functionName: "votingPeriod",
+      args: [],
+    });
+
+  const getVotingDelay = () =>
+    useReadContract({
+      ...shuffleGovernorContract,
+      functionName: "votingDelay",
+      args: [],
+    });
+
+  return { getProposalThreshold, getQuorum, getVotingDelay, getVotingPeriod };
+};
