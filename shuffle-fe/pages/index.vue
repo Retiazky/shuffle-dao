7b4b2d92627a6798788809ec1bd405b2714472ea
Branislav Hozza <brankohozza@gmail.com>
2024-05-10 15:37:00 +0200

    :sparkles: connect component

--- shuffle-fe/pages/index.vue
+++ shuffle-fe/pages/index.vue
@@ -1,11 +1,13 @@
 <template>
   <div>
     <h1>Landing page</h1>
-    <s-button @click="$router.push('/home')">Go in!</s-button>
+    <s-button @click="$router.push('/home')">Go in! {{ isConnected }}</s-button>
+    <connect-button />
   </div>
 </template>
 <script setup lang="ts">
 definePageMeta({
   layout: false,
 });
+const { isConnected } = useAccount();
 </script>
