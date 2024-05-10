9a6f396e693186b0922d82c202c8259299edb4e6
Branislav Hozza <brankohozza@gmail.com>
2024-05-10 11:36:19 +0200

    :sparkles: base setup

---
+++ shuffle-fe/components/layout/PageHeader.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <s-button type="primary" icon="plus">Hello</s-button>
+  </div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style></style>
---
+++ shuffle-fe/layouts/default.vue
@@ -0,0 +1,12 @@
+<template>
+  <page-header />
+  <div>
+    <slot />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import PageHeader from "@/components/layout/PageHeader.vue";
+</script>
+
+<style></style>
