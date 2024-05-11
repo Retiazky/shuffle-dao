d1512717ee34e9d4cc30b3234e8a6d5263eb3eb3
Branislav Hozza <brankohozza@gmail.com>
2024-05-11 13:57:40 +0200

    todo

---
+++ shuffle-fe/server/services/vote-service.ts
@@ -0,0 +1,30 @@
+
+
+
+const API_BASE = 'http://localhost:4350'
+
+
+export function voteService() {
+    const getProposals =  async () =>  await fetch(`${API_BASE}/graphql`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                query: `
+                    query {
+                        proposals {
+                            id
+                            title
+                            description
+                            votes
+                        }
+                    }
+                `,
+            }),
+        })
+        
+    }
+
+    return { getProposals }
+}
