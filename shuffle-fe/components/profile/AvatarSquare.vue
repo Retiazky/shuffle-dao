7d4be7406b423208a803d3b7372fa8009e38f6d4
Lucia Mandová <lucka.mandova@gmail.com>
2024-05-11 12:43:28 +0200

    :sparkles: Add avatar component

---
+++ shuffle-fe/components/profile/AvatarSquare.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="w-[200px] h-[200px]">
+    <img
+      src="/img/avatar2.jpg"
+      alt="avatar"
+      class="w-full h-full object-cover rounded-sm border-gradient"
+    />
+  </div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style scoped>
+.border-gradient {
+  position: relative;
+  box-shadow: 0 0 8px rgba(255, 105, 180, 0.5), 0 0 15px rgba(255, 20, 147, 0.5);
+  border-radius: 4px;
+}
+</style>
