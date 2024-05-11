bdd55be7d384da8884aa94b9f2c2e91e72aec7d4
Adam Prisenznak <adam.prisen@gmail.com>
2024-05-11 15:23:20 +0200

    index proposals add targets values calldatas

--- indexer/src/processor.ts
+++ indexer/src/processor.ts
@@ -1,5 +1,6 @@
 import {assertNotNull} from '@subsquid/util-internal'
 import * as governorAbi from './abi/governor'
+import * as daoAbi from './abi/shuffleDAO'
 import {
     BlockHeader,
     DataHandlerContext,
@@ -10,6 +11,7 @@ import {
 } from '@subsquid/evm-processor'
 
 export const GOVERNOR_CONTRACT = '0x30dbEcc57bd06B780d5274a2e74527324f372E9b'.toLowerCase()
+export const DAO_CONTRACT = '0xa08CB97D2aA395aAAe66Ab6579c630e2cAB077c9'.toLowerCase()
 
 export const processor = new EvmBatchProcessor()
     .setGateway('https://v2.archive.subsquid.io/network/base-sepolia')
@@ -32,6 +34,10 @@ export const processor = new EvmBatchProcessor()
 		address: [ GOVERNOR_CONTRACT],
 		topic0: [governorAbi.events.ProposalCreated.topic, governorAbi.events.VoteCast.topic, governorAbi.events.ProposalExecuted.topic],
 	  })
+	.addLog({
+		address: [DAO_CONTRACT],
+		topic0: [daoAbi.events.InstructorSet.topic, daoAbi.events.LessonCreated.topic, daoAbi.events.ParticipantRegistered.topic]
+	})
 
 export type Fields = EvmBatchProcessorFields<typeof processor>
 export type Block = BlockHeader<Fields>
